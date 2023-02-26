f4f711c8c8b2bbc18149b5054f8b2d79b1ac4c49
RishitMungra <mungrarishit@gmail.com>
2023-02-26 16:29:18 +0530

    Update ItemPage.dart

--- flutter_application_1/lib/pages/ItemPage.dart
+++ flutter_application_1/lib/pages/ItemPage.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 import '../widgets/ItemAppBar.dart';
 import '../widgets/ItemBottomBar.dart';
 
-class ItemPage extends StatelessWidget {
+class ItemPage extends StatefulWidget {
+  @override
+  State<ItemPage> createState() => _ItemPageState();
+}
+
+class _ItemPageState extends State<ItemPage> {
+  bool click = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,10 +44,26 @@ class ItemPage extends StatelessWidget {
                           color: Color(0xFF475269),
                         ),
                       ),
-                      Icon(
-                        Icons.favorite,
-                        color: Colors.redAccent,
-                        size: 30,
+                      Container(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              click = !click;
+                            });
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.white,
+                            shadowColor: Colors.transparent,
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.all(5),
+                            child: Icon(
+                                (click == false)
+                                    ? Icons.favorite
+                                    : Icons.favorite_border,
+                                color: Colors.red),
+                          ),
+                        ),
                       )
                     ],
                   ),
@@ -50,16 +73,21 @@ class ItemPage extends StatelessWidget {
                   child: Row(
                     // mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      for (int i = 1; i < 5; i++)
-                        Icon(
+                      RatingBar.builder(
+                        initialRating: 3,
+                        minRating: 0,
+                        direction: Axis.horizontal,
+                        allowHalfRating: true,
+                        itemCount: 5,
+                        itemSize: 30,
+                        itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                        itemBuilder: (context, _) => Icon(
                           Icons.star,
-                          size: 30,
                           color: Colors.redAccent,
                         ),
-                      Icon(
-                        Icons.star_half,
-                        size: 30,
-                        color: Colors.redAccent,
+                        onRatingUpdate: (rating) {
+                          print(rating);
+                        },
                       ),
                       Spacer(),
                       Row(
